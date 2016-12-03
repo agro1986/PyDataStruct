1c1ac54cea7957f8821fc7cce572e51b65b2e4f2
Agro Rachmatullah <agro1986@gmail.com>
2016-12-04 00:58:37 +0900

    linked list supports adding new members and count

--- pydatastruct.py
+++ pydatastruct.py
@@ -1,2 +1,27 @@
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
 class LinkedList:
-    pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    @property
+    def length(self):
+        length = 0
+        next_node = self.head
+        while next_node is not None:
+            length += 1
+            next_node = next_node.next
+        return length
+
+    def add(self, data):
+        new_node = LinkedListNode(data)
+        if self.tail is None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from pydatastruct import *
 
-l = LinkedList()
-
 print('hello')
+
+l = LinkedList()
+print(l.length)
+
+l.add(1)
+print(l.length)
+
+l.add(2)
+print(l.length)
